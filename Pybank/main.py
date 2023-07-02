da82b72ebedc96dba255fcb0349f496855e70338
Angelo Lima <aclima1988@gmail.com>
2023-07-02 15:36:01 -0400

    Completed the PyBank homework and updated the README file.

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,40 +1,121 @@
-def find_number_of_months(budget_data):
+# Import the necessary modules
+import os
+import csv
+from pathlib import Path
+import sys
 
-  # Finds the number of months in the CSV file.
-  months = 0
-  with open(budget_data, newline="") as financial_data:
+# Specify the path where the CSV file is located
+budget_data = os.path.join("/Users/angelolima/Desktop/Python_Homework/PyBank/Resources/budget_data.csv")
+
+# Set initial variables
+totalMonths = 0
+totalProfitLosses = 0
+averageChange = 0
+greatestIncrease = 0
+greatestDecrease = 0
+totalChange = 0
+
+# Use Pandas to the read the CSV file.
+#df = pd.read_csv(budget_data)
+
+# Open the csv file and read through each row 
+with open(budget_data, "r") as financial_data:
     reader = csv.reader(financial_data, delimiter=",")
 
-    # Read the header row
+    # Skip the header
     header = next(reader)
 
-    # Create empty lists to store the data from each column
-    column_1 = []
-    column_2 = []
-    column_3 = []
+    # Skip to row 2
+    row1 = next(reader)
 
+    # Create a previous variable to hold the change values that will be added to calculate totalChange.
+    previous = int(row1[1])
+
+    # Initialize the totalMonths variable from the first row
+    totalMonths += 1
+
+    # Initialize the totalProfitLosses variable with the value in the first row
+    totalProfitLosses += int(row1[1])
+
+    # Create a for loop to go through all rows
     for row in reader:
-      # Store the data from each column into the corresponding list
-      column_1.append(row[0])
-      column_2.append(row[1])
-      column_3.append(row[2])
+        
+        # Add the total number of months in the spreadsheet
+        totalMonths += 1
 
-    # Find the number of months
-    for row in column_1:
-      if isinstance(row, str):
-        if row.startswith("month"):
-          months += 1
+        # Add the values in the Profits/Losses column
+        totalProfitLosses += int(row[1])
 
-  return months
+        # Calculate the change in profits/losses
+        current = int(row[1])
+        change = current - previous
+        totalChange += change
 
-if __name__ == "__main__":
-  budget_data = "budget_data.csv"
-  number_of_months = find_number_of_months(budget_data)
+        # Locate the greatest increase in profits
+        if change > greatestIncrease:
+            greatestIncrease = change
 
-  # Print the number of months
-  print("Number of months:", number_of_months)
+            # Locate the month equivalent to the greatest increase 
+            greatestIncrease_index = row[0]
 
-  # Print each column of data
-  print("Column 1:", column_1)
-  print("Column 2:", column_2)
-  print("Column 3:", column_3)
+        # Check if the current change is less than the greatest decrease
+        if change < greatestDecrease:
+            greatestDecrease = change
+
+            # Locate the month equivalent to the greatest increase 
+            greatestDecrease_index = row[0]
+
+        
+
+        # Update the previous value for the next iteration
+        previous = current
+
+# Calculate the average change
+averageChange = round(totalChange / (totalMonths - 1), 2)
+
+print("Financial Analysis")
+print("----------------------------")
+
+# Print out the total months
+print(f"Total Months: {totalMonths}")
+
+# Print out the total profit losses
+print(f"Total: ${totalProfitLosses}")
+
+# Print out the average changes
+print(f"Average Change: ${averageChange}")
+
+# Print out the greatest increase in profit and the month when it happened
+print (f"Greatest Increase in Profits: {greatestIncrease_index} (${greatestIncrease})")
+
+# Print out the greatest decrease in profit and the month when it happened
+print (f"Greatest Decrease in Profits: {greatestDecrease_index} (${greatestDecrease})")
+
+
+# Save and export a text file of the output of my code
+# Find the folder in which the csv file is saved and assign it to the current_dir variable
+current_dir = os.path.dirname("/Users/angelolima/Desktop/Python_Homework/PyBank/analysis")
+
+# Join the path to the folder holding the CSV file and the analysis folder and assign the path to the variable output_dir
+output_dir = os.path.join(current_dir, "analysis")
+
+# Combine the path to the output_dir folder with the string PyBankOutput.text to create a new path to the file named PyBankOutput.text and assign it to the filename variable
+filename = os.path.join(output_dir, "PyBankOutput.txt")
+
+# Use the filename variable to open the path where the output text will be stored.
+with open(filename, "w") as f:
+
+    # Redirect the standard output to the filename instead of the terminal
+    sys.stdout = f
+
+    # Write the output to the file
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months: {totalMonths}")
+    print(f"Total: ${totalProfitLosses}")
+    print(f"Average Change: ${averageChange}")
+    print(f"Greatest Increase in Profits: {greatestIncrease_index} (${greatestIncrease})")
+    print(f"Greatest Decrease in Profits: {greatestDecrease_index} (${greatestDecrease})")
+    
+    # Restore the standard output back to the terminal
+    sys.stdout = sys.__stdout__
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Python Homework
+
+I imported the necessary modules.
+Specified the path where the CSV file is located.
+After that I defined the initial variables and set them to zero.
+I then, asked the python to ppen the csv file saved in the 'Resources' folder and read through each row of the CVS file.
+I instructed python to skip the header row since it doesn't contain data.
+I then instructed python to skip row 1 of the CSV file and go directly to row 2, in order to be able to calculate then change of profit/losses from one month to the next.
+I create a 'previous' variable to hold the change values that will be added to calculate totalChange.
+I initialized the totalMonths variable from the first row to loop through all rows.
+I initialized the totalProfitLosses variable with the value in the first row of the CSV file.
+
+I created a for loop to go through all rows in the CSV file.
+
+Inside the for loop:
+I calculated the total number of months in the spreadsheet
+I ihad the totalProfitLosses variable loop through the Profits/Losses column
+I then calculate the change in profits/losses from one month to the next by creating a current variable that starts on row 2 of the CSV file then subtracts the value in that cell from the value of the of the cell prior to it, and loops through all cells performing this operation until it runs out of rows.
+I then asked python to locate the greatest increase in profits by looping through the change values that came out of the calculation above and finding the highest of the values and find the Date row for the month equivalent to the hgihest value.
+I did the same as bove for the lowest profit increase/highest profit decrease and also locate the month equivalente to that value.
+I updated the value of the 'previous' variable with the new value of each change in profit calculation for the next iteration in the loop.
+
+Outside of the foor loop:
+I calculate the average change by deviding the totalChange in profits by the total number of months and rounded the return value to the second decimal point.
+
+I then instructed python to print to the terminal, the title of the output file, the total number of months, the total profit, the average changes in profit, the greatest increase in profits and the month when it happened, and the lowest increase/highest decrease in profits and the month when it happened.
+
+I then instructed python to export a text file with the title of the output file, the total number of months, the total profit, the average changes in profit, the greatest increase in profits and the month when it happened, and the lowest increase/highest decrease in profits and the month when it happened.
+    The program did that by finding the folder where the CSV file was located and assigning the path to the 'current_dir' variable. Then joining the path to the folder holding the CSV file and the analysis folder and assigning that path to the variable 'output_dir'. After that it combined the path to the output_dir folder with the string PyBankOutput.text to create a new path to the file named PyBankOutput.text, and assigning the path to the 'filename' variable. The code then used the 'filename' variable to open the path where the output text will be stored. The sys.stdout function redirects the standard output to the 'filename' variable instead of the terminal which is the automatic location where outputs are usually printed out to. It then uses print() statements top write the output to the text file and it finishes by restoring the standard output location back to the terminal.
